a2c52c5550a62ad63d84231e1c7a5e6586965629
cseh17 <krisztian.cseh@smail.th-koeln.de>
2018-08-09 18:23:08 +0200

    Bugfix: - misspelling corrections

--- app/src/main/java/com/atm_search/cseh_17/geld_kompass/SearchFor.java
+++ app/src/main/java/com/atm_search/cseh_17/geld_kompass/SearchFor.java
@@ -5,7 +5,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
